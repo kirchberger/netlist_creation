1202cfdf92c258af91fe8151a4bb93ed54e6a228
kirchberger <kirchbergeralex0@gmail.com>
2025-09-09 19:02:27 +0000

    test commit

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+	printf("file location is: %s\n",argv[1]);
+	
+	FILE *ptr_net;
+	ptr_net = fopen(argv[1],"r");
 
+	
+	char buffer[255];
+	while(fgets(buffer,255,ptr_net) != NULL){
+		printf("%s\n", buffer);
+	}
+	
+	fclose(ptr_net);
 	printf("Finished \n");
 	return 0;
 }
