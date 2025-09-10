7ec223bfabadc2f6fd6534a931ffd82686e6a33e
kirchberger <kirchbergeralex0@gmail.com>
2025-09-10 16:34:45 +0000

    more

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # netlist_creation
 
 Simple tool aiding a circuit simulator
+
+This tool seems very similar to a compiler where there needs to be lexical analysis, parsing and then system generation.
--- main.c
+++ main.c
@@ -2,23 +2,49 @@
 #include <string.h>
 #include <stdlib.h>
 
+void lexor (char* fileLoc){
+	FILE *file;
+	file = fopen(fileLoc,"r");
 
-int main(int argc, char* argv[]) {
-	printf("file location is: %s\n",argv[1]);
-	
-	FILE *ptr_net;
-	ptr_net = fopen(argv[1],"r");
-
-	
-	char buffer[255];
-	while(fgets(buffer,255,ptr_net) != NULL){
-		char* piece = strtok(buffer," ");
-		if (piece != "\n"){
-			printf("%s", strlen(piece));
+	char buff[100];
+	char* token;
+	while(fgets(buff,100,file) != NULL){
+		//printf("line: %s", buff);
+		token = strtok(buff," ");
+		while(token != 0){
+			if (*token != '\n'){
+				printf("%s", token);
+			}
+			token = strtok(0," ");
 		}
 	}
+
+	fclose(file);
+}
+
+int main(int argc, char* argv[]) {
+	if (argc<2){
+		printf("No file location\n");
+		return 1;
+	}
+
+	if (argc>3){
+		printf("Too many arguments\n");
+		return 1;
+	}
+	printf("file location is: %s\n",argv[1]);
 	
-	fclose(ptr_net);
+	lexor(argv[1]);
+	/*
+	char buffer[255];
+	while(fgets(buffer,255,ptr_net) != NULL){
+		// First Character
+		char* piece = strtok(buffer," ");
+		if (*piece != '\n'){
+			switch (*piece[0]
+		}
+
+	}*/
 	printf("Finished \n");
 	return 0;
 }
